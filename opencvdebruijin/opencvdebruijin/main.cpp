2f23d74ae449b90c4e2fe76727526ecf8f32e6a0
liuliu <leoliu@u.northwestern.edu>
2014-04-16 23:37:13 -0500

    Initial Commit

---
+++ opencvdebruijin/opencvdebruijin/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencvdebruijin
+//
+//  Created by leoliu on 4/16/14.
+//  Copyright (c) 2014 leoliu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
